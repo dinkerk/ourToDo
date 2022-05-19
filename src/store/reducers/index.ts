fd2946b6f3bf18ae7da64d0edaa5fe2936a667ba
DmitrievAS <dmitrieffsasha@yandex.ru>
2022-05-19 10:44:20 +0300

    4. Готовый проект TodoList: Typescript c Redux и Redux-Thunk.  Next step: 1. Перевести на БД Firebase 2. Добавить возможность авторизации пользователя 3. введение задач и проставления чек бокса о выполнении.

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,7 +1,9 @@
 import {combineReducers} from "redux";
 import {userReducer} from "./userReducer";
+import {todoReducers} from "./todoReducers";
 
 export const rootReducer = combineReducers({
-    user: userReducer
+    user: userReducer,
+    todo: todoReducers
 })
 export type RootState = ReturnType<typeof rootReducer>
